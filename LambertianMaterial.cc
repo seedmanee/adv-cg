b37f89f3fdee7df92b56efd13b3e8d63808ce7b0
jcwu <seedmanee@gmail.com>
2011-10-09 14:27:06 -0700

    recursive ver 1

--- LambertianMaterial.cc
+++ LambertianMaterial.cc
@@ -23,13 +23,13 @@ LambertianMaterial::~LambertianMaterial()
 }
 
 void LambertianMaterial::shade(Color& result, const RenderContext& context,
-                               const Ray& ray, const HitRecord& hit, const Color&, int) const
+                               const Ray& ray, const HitRecord& hit, const Color& atten, int depth) const
 {
   const Scene* scene = context.getScene();
   const vector<Light*>& lights = scene->getLights();
-  Point hitpos = ray.origin()+ray.direction()*hit.minT();
+  Point hitpos = ray.origin()+ray.direction()*hit.minT(); // ray hit object at point hitpos
   Vector normal;
-  hit.getPrimitive()->normal(normal, context, hitpos, ray, hit);
+  hit.getPrimitive()->normal(normal, context, hitpos, ray, hit); // normal at hitpos
   double costheta = Dot(normal, ray.direction());
   if(costheta > 0)
     normal = -normal;
@@ -38,30 +38,44 @@ void LambertianMaterial::shade(Color& result, const RenderContext& context,
 	
   Color light = scene->getAmbient()*Ka;
 	
-#if 0
-  for(vector<Light*>::const_iterator iter = lights.begin(); iter != lights.end(); iter++){
-#else
-    Light*const* begin = &lights[0];
-    Light*const* end = &lights[0]+lights.size();
-    while(begin != end){
-#endif
-			Color light_color;
-			Vector light_direction;
-			double dist = (*begin++)->getLight(light_color, light_direction, context, hitpos);
-			double cosphi = Dot(normal, light_direction);
-			if(cosphi > 0){
-				// Cast shadow rays...
-				HitRecord shadowhit(dist);
-				Ray shadowray(hitpos, light_direction);
-				world->intersect(shadowhit, context, shadowray);
-				if(!shadowhit.getPrimitive())
-					// No shadows...
-					light += light_color*(Kd*cosphi);
+	Color Is(0.0, 0.0, 0.0);
+	Color bounce(0.0, 0.0, 0.0);
+	const double atten_factor = 0.7;
+	Vector reflection_ray_direction = 2 * Dot(-ray.direction(), normal) * normal + ray.direction();
+	Ray reflection_ray(hitpos, reflection_ray_direction);
+
+	Light*const* begin = &lights[0];
+	Light*const* end = &lights[0]+lights.size();
+	
+	while(begin != end){   // loop over all light source
+
+		Color light_color;
+		Vector light_direction;
+		double dist = (*begin++)->getLight(light_color, light_direction, context, hitpos);
+		double cosphi = Dot(normal, light_direction);
+		
+		if(cosphi > 0){
+			// Cast shadow rays...
+			HitRecord shadowhit(dist);
+			Ray shadowray(hitpos, light_direction);
+			world->intersect(shadowhit, context, shadowray);
+			if(!shadowhit.getPrimitive())
+			{
+				// No shadows...
+				light += light_color*(Kd*cosphi);
+				
+				Vector reflection_light_direction = 2 * Dot(light_direction, normal) * normal - light_direction;
+				Is += light_color * Ks * pow( max(Dot(reflection_light_direction, -ray.direction()), 0.0), alpha);
 			}
 		}
-		result = light*color;
-	}
+	} // end of while(begin != end)
 	
+	// add recursive part
+	Color reflection_result;
+	context.getScene()->traceRay(reflection_result, context, reflection_ray, atten * atten_factor, depth + 1);
+	result = light*color + Is + reflection_result * atten_factor;
+}
+
 /*
 void LambertianMaterial::shade(Color& result, const RenderContext& context,
                                const Ray& ray, const HitRecord& hit, const Color& atten, int depth) const
